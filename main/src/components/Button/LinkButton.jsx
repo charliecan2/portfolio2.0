12b519bea74f77f13d4a1afcc954aa9595c7b965
ccantu20 <carloscq920@gmail.com>
2021-07-23 16:54:00 -0500

    Created a button component used to link to external sites and to download my resume

---
+++ main/src/components/Button/LinkButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './LinkButton.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+function LinkButton({ href, text, faIcon }) {
+    return (
+        <a className="link-btn" href={href} type="button" download target="_blank">
+            <FontAwesomeIcon className="faIcon" icon={faIcon} size="lg"/> {text}
+        </a>
+    )
+}
+
+export default LinkButton
