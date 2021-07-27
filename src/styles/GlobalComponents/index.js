19807f49cad40a238d008be145aa336823c47ad7
ccantu20 <carloscq920@gmail.com>
2021-07-27 15:46:02 -0500

    Changed the resume button to a button element instead of an anchor tag

--- src/styles/GlobalComponents/index.js
+++ src/styles/GlobalComponents/index.js
@@ -166,7 +166,7 @@ export const SecondaryBtn = styled.button`
   }
 `
 
-export const ButtonBack = styled.a`
+export const ButtonBack = styled.button`
   width: ${({ alt }) => alt ? '150px' : '262px'};
   height: ${({ alt }) => alt ? '52px' : '64px'};
   border-radius: 50px;
@@ -199,7 +199,7 @@ export const ButtonBack = styled.a`
   }
 `
 
-export const ButtonFront = styled.a`
+export const ButtonFront = styled.button`
   border: none;
   border-radius: 50px;
   color: #fff;
