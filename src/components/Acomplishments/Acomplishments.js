2127b7d53840936bfbf43efafaa3266329547586
Carlos Cantu <carloscq920@gmail.com>
2023-02-13 17:08:37 -0600

    Updated Timeline and Accomplishments components

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -4,16 +4,26 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
+  { header: "Bootcamp", text: 'Completed UT Austin\'s rigurous Coding Bootcamp in Spring 2021'},
+  { header: "First Web App", text: 'Made my first front-end web application, called Night Out, in a team of three developers.' },
+  { header: "Full-Stack MERN App", text: 'Created a more complicated Full-Stack MERN application, called ResuMake, with a team of five developers in the bootcamp cohort.' },
+  { header: "Solo Learning", text: 'Learned a number of web technologies post bootcamp, such as Redux and NextJS, to build more applications.' },
 ];
 
 const Acomplishments = () => (
-  <div>
-    Acomplishments
-  </div>
+  <Section>
+    <SectionTitle>
+      Acheivements
+    </SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{card.header}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
 );
 
 export default Acomplishments;
--- src/components/Acomplishments/AcomplishmentsStyles.js
+++ src/components/Acomplishments/AcomplishmentsStyles.js
@@ -25,7 +25,7 @@ export const Boxes = styled.div`
 export const Box = styled.div`
   background: #212D45;
   border-radius: 12px;
-  height: 144px;
+  height: 100%;
   padding: 24px;
   @media ${props => props.theme.breakpoints.lg} {
     height: 210px;
@@ -33,16 +33,16 @@ export const Box = styled.div`
   }
 
   @media ${props => props.theme.breakpoints.md} {
-    height: 135px;
+    height: 100%;
     padding: 16px;
   }
 
   @media ${props => props.theme.breakpoints.sm} {
-    height: 110px;
+    height: 100%;
     padding: 12px;
     
     &:nth-child(2n){
-      grid-row:2;
+      grid-row:4;
     }
   }
 `
